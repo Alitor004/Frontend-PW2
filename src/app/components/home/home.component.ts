307474059c93e4d83087f8dc6a69cc05885d632c
Alitor004 <irmaosgomes1302@gmail.com>
2023-05-11 22:07:37 -0300

    Array/Vetor usando o 'for' para escrever.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,16 +8,19 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   nomeProduto : string = 'Cusdo de Angular';
   anuncio : string = `O ${this.nomeProduto} está em promoção`;
-  idProduto : number = 3;
+  idProduto : number = 1;
   precoProduto : number = 2.59;
   promocao : boolean = true;
 
   logo : string = "../../../assets/img/logo_horacio.png";
   foto: string = '../../../assets/img/crud.png';
 
-  dataValidade = '2021-12-31'
+  dataValidade = '2021-12-31';
+
 
   constructor() {
+
+
 
   }
 
--- src/app/components/produtos/lista-produtos/lista-produtos.component.ts
+++ src/app/components/produtos/lista-produtos/lista-produtos.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class ListaProdutosComponent {
 
+  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumeros: number[] = [15, 15.18, 100];
+
+  objetoModelo = {
+    nome: 'Fatima',
+    idade: 18,
+    altura: 1.56,
+    graduado: true
+  };
+
+
+  listaProdutos: any[] = [
+    {nome: 'Cusdo de Angular', precoProduto: 35.56, validade: '2021/12/31', id: 1},
+    {nome: 'Cusdo de Ionic', precoProduto: 50, validade: '2021-12-31', id: 2, promocao: true},
+    {id: 3, nome: 'Cusdo de Ionic Avançado', precoProduto: 50, validade: '2021/12/31'},
+  ];
+
+  contructor(){
+    for (let item of this.listaStrings){
+      console.log(item);
+    }
+
+    for (const item of this.listaNumeros){
+      console.log(item);
+    }
+
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
+
+  }
+
+
 }
