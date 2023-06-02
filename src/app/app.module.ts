3c20e611908248da111ea10c69eb65243fa45b40
Alitor004 <irmaosgomes1302@gmail.com>
2023-06-01 22:34:22 -0300

    Instalações concluidas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import { registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 registerLocaleData(localePt);
 
 @NgModule({
@@ -32,7 +35,9 @@ registerLocaleData(localePt);
     AppRoutingModule,
     FormsModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [
     {provide : LOCALE_ID, useValue : 'pt-BR' },
--- src/app/services/prudutos.service.ts
+++ src/app/services/prudutos.service.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { IProduto } from './../components/model/IProduto.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -9,11 +10,16 @@ import { Observable } from 'rxjs';
 export class ProdutosService {
   private URL: string = 'http://localhost:3000/prudutos';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private toastr: ToastrService) {
 
   }
 
   buscarTodos(): Observable<IProduto[]>{
     return this.http.get<IProduto[]>(this.URL);
   }
+
+  exibirMensagem(titulo: string, mensagem: string, tipo: string): void {
+    this.toastr.show(mensagem,titulo, {closeButton:true, progressBar: true}, tipo);
+  }
+
 }
